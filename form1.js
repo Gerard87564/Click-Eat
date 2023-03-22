922148840da7044db5d9a42ef926d46a991a014a
Gerard87564 <115035588+Gerard87564@users.noreply.github.com>
2023-03-22 19:15:30 +0100

    Add files via upload

--- form1.js
+++ form1.js
@@ -5,7 +5,7 @@ function validate(){
 	var password = document.getElementById("password").value;
 	if ( username == "Beavie" && password == "8261819A*"){
 	alert ("Login successfull!");
-	window.open('https://www.jobeaver.cat/index.html', '_blank');
+	window.open('http://www.jobeaver.cat/index.html', '_blank');
 	return false;
 	}
 
