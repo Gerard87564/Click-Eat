899964d3653d5c563c30775e06390e41c6f5d12e
Gerard87564 <115035588+Gerard87564@users.noreply.github.com>
2023-03-22 17:27:31 +0100

    Add files via upload

---
+++ form1.js
@@ -0,0 +1,25 @@
+var attempt="5";
+function loadHomePage() {
+	window.location.assign = "https://www.jobeaver.cat";
+}
+
+function validate(){
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	if ( username == "Beavie" && password == "8261819A*"){
+	alert ("Login successfull!");
+	return false;
+	}
+
+	else{
+
+	attempt --;
+	alert("You have "+attempt+" attempts;");
+	if( attempt == 0){
+		document.getElementById("user_name").disabled = true;
+		document.getElementById("password").disabled = true;
+		document.getElementById("submit").disabled = true;
+		return false;
+}
+}
+}
