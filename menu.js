ceb8fcc637a93ff39d77f8ff4cc8f579a6ae8007
Gerard87564 <115035588+Gerard87564@users.noreply.github.com>
2023-05-09 00:21:39 +0200

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,3 +1,4 @@
+console.log("1")
 jQuery("document").ready(function($){
 
     var menuButton = $(".menu-icon");
@@ -9,6 +10,18 @@ jQuery("document").ready(function($){
             menu.removeClass("htopnav");
         } else {
             menu.addClass("htopnav");
+        }
+    });
+
+    var menuButton2 = $("#icon-lupa"); 
+        menu2 = $("div.nav div");  
+
+    menuButton2.click(function() {
+
+        if(menu2.hasClass("show")) {
+            menu2.removeClass("show");
+        } else {
+            menu2.addClass("show");
         }
     });
 });
