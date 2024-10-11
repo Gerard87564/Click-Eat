d125e4a6ceacd1cbf7b0186f3682ab978edc1beb
Gerard87564 <115035588+Gerard87564@users.noreply.github.com>
2024-10-11 12:28:25 +0200

    Add files via upload

--- Projecte/JS/script.js
+++ Projecte/JS/script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const icon= document.getElementById('hide');
+    const menu= document.getElementById('hamburger-menu');
     const htopnav = document.getElementById('htopnav');
+    const icon= document.getElementById('bar');
 
-    icon.addEventListener("click", function () {
+    menu.addEventListener("click", function () {
+        icon.classList.toggle('animate');
         htopnav.classList.toggle('visible');
     }); 
 });
