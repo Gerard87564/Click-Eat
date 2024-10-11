65c882a0aa4ca028c017cc180c6b2ea614e27034
Gerard87564 <115035588+Gerard87564@users.noreply.github.com>
2024-10-11 11:24:41 +0200

    Add files via upload

--- Projecte/JS/script.js
+++ Projecte/JS/script.js
@@ -6,15 +6,3 @@ document.addEventListener('DOMContentLoaded', function () {
         htopnav.classList.toggle('visible');
     }); 
 });
-
-document.querySelector("#hide").addEventListener("click", animateBars);
-
-var line1= document.querySelector(".line1__bars-menu");
-var line2= document.querySelector(".line2__bars-menu");
-var line3= document.querySelector(".line3__bars-menu");
-
-function animateBars() {
-    line1.classList.toggle("activeline1__bars-menu");
-    line2.classList.toggle("activeline2__bars-menu");
-    line3.classList.toggle("activeline3__bars-menu");
-}
