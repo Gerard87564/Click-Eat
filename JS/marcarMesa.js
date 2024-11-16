6c00bf79bc182ac32ef107cf45c8372c5557604d
Gerard87564 <115035588+Gerard87564@users.noreply.github.com>
2024-11-16 22:00:17 +0100

    Add files via upload

--- JS/marcarMesa.js
+++ JS/marcarMesa.js
@@ -6,20 +6,31 @@ document.addEventListener("DOMContentLoaded", function () {
             if (mesa.classList.contains('mesa-ocupada')) {
                 alert("Esta mesa ya está ocupada.");
             } else {
-                fetch('http://api.clickeat.cat/marcar-mesa-ocupada', {
+                setTimeout(function () {
+                    localStorage.removeItem(`mesa_${mesa.id}`);
+
+                    mesa.classList.remove('mesa-ocupada');
+                    mesa.innerHTML = "";
+                }, 30 * 60 * 1000);
+
+                fetch('https://api.clickeat.cat/marcar-mesa-ocupada', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify({ mesaid: mesa.id })  
+                    body: JSON.stringify({ mesaid: mesa.id })
                 })
                 .then(response => response.json())
                 .then(data => {
                     if (data.success) {
                         mesa.classList.add('mesa-ocupada');
                         mesa.innerHTML = "<p>Ocupada</p>";
-
-                        window.location.href = `carta.html?mesa=${mesa.id}`;  
+                        
+                        let mesasOcupadas = JSON.parse(localStorage.getItem('mesasOcupadas')) || [];
+                        mesasOcupadas.push(mesa.id); 
+                        localStorage.setItem('mesasOcupadas', JSON.stringify(mesasOcupadas));
+                
+                        window.location.href = `carta.html?mesa=${mesa.id}`;
                     } else {
                         alert("Hubo un problema al marcar la mesa.");
                     }
@@ -27,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 .catch(error => {
                     console.error("Error al marcar la mesa como ocupada:", error);
                     alert("Hubo un error al procesar la solicitud.");
-                });
+                });                
             }
         });
     });
